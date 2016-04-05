4f8dab7a9f37c3078ce4c4ffd9f34e461c14a586
kavyaNarne <kavyanarne@siu.edu>
2016-04-05 04:06:55 -0500

    admin removes image

---
+++ del.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class del : System.Web.UI.Page
+{
+    String connStr = ConfigurationManager.ConnectionStrings["VRConnectionString"].ConnectionString;
+    SqlConnection con;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            try
+            {
+                con = new SqlConnection(connStr);
+                con.Open();
+                String prm = Request.QueryString["prm"];
+                SqlCommand cmd = new SqlCommand("delete from catalog where pid=" + prm, con);
+                cmd.ExecuteNonQuery();
+                Label1.Text = "File removed successfully!";
+            }
+            catch (Exception ee)
+            {
+                Label1.Text="Check file!";
+                Response.Write(ee.Message);
+            }
+            con.Close();
+        }
+    }
+}
